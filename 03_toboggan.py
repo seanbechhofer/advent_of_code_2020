d26c27a21098b734ef426062daedd65306fcb6ae
Sean Bechhofer <sean.bechhofer@manchester.ac.uk>
2020-12-03 22:28:54 +0000

    Day 3, Part 2

--- 03_toboggan.py
+++ 03_toboggan.py
@@ -39,7 +39,10 @@ class TestStuff(unittest.TestCase):
     def test_five(self):
         point = (0,1)
         self.assertTrue(tree(point,self.mmap()))
-        
+
+    def test_six(self):
+        self.assertEqual(part_two(self.mmap()), 336)
+
 
 def debug(arg):
 #    pass
@@ -50,6 +53,10 @@ def debug(arg):
 def move_one(point):
     x,y = point
     return (x+3, y+1)
+
+def move_two(point,a,b):
+    x,y = point
+    return (x+a, y+b)
 
 def square(point, lines):
     x,y = point
@@ -70,10 +77,29 @@ def part_one(lines):
             trees += 1
         x,y = move_one((x,y))
     return trees
-    
+
+def check_two(lines,a,b):
+    debug(len(lines))
+    trees = 0
+    x,y = (0,0)
+    while y < len(lines):
+        debug("{} {}".format((x,y), square((x,y), lines)))
+        if tree((x,y), lines):
+            trees += 1
+        x,y = move_two((x,y),a,b)
+    return trees
+
+def part_two(lines):
+    return (check_two(lines,1,1) *
+                check_two(lines,3,1) *
+                check_two(lines,5,1) *
+                check_two(lines,7,1) *
+                check_two(lines,1,2))
+
 if __name__=='__main__':
 #    unittest.main()
     lines = []
     for line in sys.stdin:
         lines.append(line.strip())
     print(part_one(lines))
+    print(part_two(lines))
